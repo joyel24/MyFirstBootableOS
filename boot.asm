42def602d290559d421c957fc326a835ec31f9f6
joyel24 <joel.martin.2104@gmail.com>
2026-02-06 12:37:27 +0100

    my first printed character

--- boot.asm
+++ boot.asm
@@ -1,7 +1,13 @@
 [bits 16]
 [org 0x7c00]
 
+mov ah, 0xe     ; https://en.wikipedia.org/wiki/INT_10H (Teletype output)
+mov al, "A"     ; character
+mov bl, 0xff    ; color
+mov bh, 0       ; page number
+int 10h         ; https://en.wikipedia.org/wiki/INT_10H (10h interrupt)
+
 jmp $
 
-times 510-($-$$) db 0
-dw 0xaa55
+times 510-($-$$) db 0       ; MBR 512
+dw 0xaa55                   ; MBR 512
