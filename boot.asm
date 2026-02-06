f0709330962e2fa832e96722c842f34cf35b6cc8
joyel24 <joel.martin.2104@gmail.com>
2026-02-06 14:25:57 +0100

    set videomode to 640x480 256colors + colors test

--- boot.asm
+++ boot.asm
@@ -1,12 +1,10 @@
 [bits 16] ; 8086
 [org 0x7c00]
 
-;mov ah, 0xe     ; https://en.wikipedia.org/wiki/INT_10H (Teletype output AH=0Eh or AH=0xE)
-;mov al, "A"     ; character
-;mov bl, 0xf     ; color
-;mov bh, 0       ; page number
-;int 10h         ; https://en.wikipedia.org/wiki/INT_10H (10h interrupt)
-
+mov ah, 0           ; >set video mode https://instrum.org/inter/int1000.htm
+mov al, 0x13        ; >to 13h (640X480 256colors)
+mov bl, 0           ;
+int 10h
 
 mov si, hello ; move hello content to si register
 print:
@@ -14,16 +12,16 @@ print:
     inc si          ; increment si
     cmp al, 0       ; >if al value eq 0
     je end          ; >jump to end
-    mov ah, 0xe     ; https://en.wikipedia.org/wiki/INT_10H (Teletype output)
-    mov bl, 0xf     ; https://en.wikipedia.org/wiki/INT_10H (Teletype output)
-    mov bh, 0       ; https://en.wikipedia.org/wiki/INT_10H (Teletype output)
-    int 10h         ; https://en.wikipedia.org/wiki/INT_10H (Teletype output)
+    mov ah, 0xe     ; https://en.wikipedia.org/wiki/INT_10H
+    inc bl          ; change color every character
+    mov bh, 0       ; https://en.wikipedia.org/wiki/INT_10H
+    int 10h         ; interrupt https://en.wikipedia.org/wiki/INT_10H
     jmp print
 end:
 
 jmp $               ; infinite loop
 
-hello: db "Hey! Hello from asm via bios", 0
+hello: db "Hey! Hello from asm via bios 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", 0
 
 times 510-($-$$) db 0       ; MBR 512
 dw 0xaa55                   ; MBR 512
